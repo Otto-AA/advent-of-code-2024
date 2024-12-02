56c72a7e3acc47da59e4526d3e948979b8e18a78
A_A <21040751+Otto-AA@users.noreply.github.com>
2024-12-02 14:10:50 +0100

    rust: day 1

---
+++ rust/1/src/main.rs
@@ -0,0 +1,97 @@
+use regex::Regex;
+use std::{
+    collections::HashMap,
+    env::args,
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    iter::zip,
+    path::Path,
+};
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    let path: &Path = Path::new(args.get(1).expect("Please specify the input path"));
+
+    let (left, right) = read_input(&path).unwrap();
+
+    println!("Distance: {}", distance(&left, &right));
+    println!("Similarity: {}", similarity(&left, &right));
+}
+
+fn read_input(path: &Path) -> Result<(Vec<i32>, Vec<i32>), Error> {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines().flatten() {
+        let x: Vec<&str> = Regex::new(r"\s+").unwrap().split(&line).collect();
+        if x.len() != 2 {
+            println!("{:?}", x);
+            panic!("Unexpected length: {}", x.len());
+        }
+        left.push(x.get(0).unwrap().parse().unwrap());
+        right.push(x.get(1).unwrap().parse().unwrap());
+    }
+
+    Ok((left, right))
+}
+
+fn distance(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut left = left.clone();
+    let mut right = right.clone();
+
+    left.sort();
+    right.sort();
+
+    zip(left, right).map(|(a, b)| (a - b).abs()).sum()
+}
+
+fn similarity(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut right_counts: HashMap<i32, i32> = HashMap::new();
+
+    for id in right {
+        *right_counts.entry(*id).or_default() += 1;
+    }
+
+    left.iter()
+        .filter(|n| right_counts.contains_key(n))
+        .map(|n| n * right_counts.get(n).unwrap())
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_sample_input() {
+        let path = Path::new("sample.txt");
+
+        let (left, right) = read_input(path).unwrap();
+
+        assert_eq!(left, vec![3, 4, 2, 1, 3, 3]);
+        assert_eq!(right, vec![4, 3, 5, 3, 9, 3]);
+    }
+
+    #[test]
+    fn distance_sample() {
+        let left = vec![3, 4, 2, 1, 3, 3];
+        let right = vec![4, 3, 5, 3, 9, 3];
+
+        let distance = distance(&left, &right);
+
+        assert_eq!(distance, 11);
+    }
+
+    #[test]
+    fn similarity_sample() {
+        let left = vec![3, 4, 2, 1, 3, 3];
+        let right = vec![4, 3, 5, 3, 9, 3];
+
+        let similarity = similarity(&left, &right);
+
+        assert_eq!(similarity, 31);
+    }
+}
